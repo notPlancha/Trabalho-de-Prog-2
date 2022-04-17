ac0b7e24c2c9d8c7fb12196590e0438b8ed7161e
afonsosilvax <afonsosilvax@sapo.pt>
2022-04-17 09:50:08 +0100

    Update Testes_ao_codigo.py

--- Testes_ao_codigo.py
+++ Testes_ao_codigo.py
@@ -85,13 +85,13 @@ def verTest(lista_normal, indice):
         try:
             dll1.ver(indice)
             return False
-        except IndexError:
-            return True
         except:
             return False
+
     try:
-        return value == dll1.ver(indice)
-    except IndexError:
+        value2 = dll1.ver(indice)
+        return value == value2
+    except:
         return False
 
 def remTest(lista_normal, item):
@@ -99,15 +99,14 @@ def remTest(lista_normal, item):
     dll1 = dll1.rem(item)
 
     try:
-        lista_inc = lista_normal.remove(item)
+        lista_normal.remove(item)
+
         if dll1 == False:
             return False
         dll1 = fromDllToList(dll1)
-        return dll1 == lista_inc
+        return dll1 == lista_normal
 
     except:
-        if dll1 == False:
-            return True
         return False
 
 def limparTest(lista_normal):
@@ -123,11 +122,11 @@ def vaziaTest():
     dll = DoublyLinkedList()
     return dll.vazia()
 
-#test = {'ins(item)': insTest(lista_norm), 'len()': lenTest(), 'mostrar()': mostrarTest()}#,
-        #'existe(item)': existeTest(valor), 'ver(p)': verTest(indice), 'ordenar()': '?', 'rem(item)': '?'
-    #, 'limpar()': limpartest(), 'vazia()': vaziatest()}
+test = {'ins(item)': insTest(lst_norm), 'len()': lenTest(lst_norm), 'mostrar()': mostrarTest(lst_norm),
+        'existe(item)': existeTest(lst_norm, vlr), 'ver(p)': verTest(lst_norm, ind), 'rem(item)': remTest(lst_norm, vlr),
+        'limpar()': limparTest(lst_norm), 'vazia()': vaziaTest()}
 
-#for i in test:
-    #print(f'{i} --> {test[i]}')
+for i in test:
+    print(f'{i} --> {test[i]}')
 
-
+dll = fromListToDll([1,2])
